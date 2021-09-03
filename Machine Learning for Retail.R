e86aa9645809b1356822ba2b774e855e6731ad8e
Andi Nur Inayah <86001320+andinurinayah@users.noreply.github.com>
2021-09-03 09:38:07 +0800

    Update Machine Learning for Retail.R

--- Machine Learning for Retail.R
+++ Machine Learning for Retail.R
@@ -5,22 +5,19 @@ transaksi_tabular <- read.transactions(file="https://storage.googleapis.com/dqla
 
 
 # Insight Top 10
-library(arules)
 data_item <- itemFrequency(transaksi_tabular, type = "absolute")
 data_item <- sort(data_item, decreasing = TRUE)
 data_item <- data_item[1:10]
 data_item <- data.frame("Nama Produk" = names(data_item), "Jumlah" = data_item, row.names = NULL)
-
 print(data_item)
 
-#bottom 10
+# Bottom 10
 data_bottom <- itemFrequency(transaksi_tabular, type = "absolute")
 data_bottom <- sort(data_bottom, decreasing = FALSE)
 data_bottom <- data_bottom[1:10]
 data_bottom <- data.frame("Nama Produk" = names(data_bottom), "Jumlah" = data_bottom, row.names = NULL)
 write.csv(data_bottom, file = "bottom10_item_retail.txt")
 print(data_bottom)
-
 
 # Kombinasi Produk Penjualan yang Menarik
 mba <- apriori(transaksi_tabular,parameter = list(supp = 10/length(transaksi_tabular), 
@@ -42,6 +39,3 @@ B <- head(sort(L, by="lift", decreasing=TRUE),n=3L)
 
 AB <- c(A,B)
 inspect(AB)
-
-
-
